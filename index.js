de456969026a4abe2f25c048f82a4af391229775
Kent1an <qkent@anamosa.k12.ia.us>
2026-02-05 17:44:09 +0000

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+var firstName = "John";
+var jobTitle = "Software Engineer";
+var city = "San Francisco";
+
+var newJobPost = `${firstName} is a ${jobTitle} in ${city}.`;
+
+document.getElementById("result").innerHTML = newJobPost;
